3e1046ea280419fc1968f1bcbdaf0baf19360670
linxin-java <2451238975@qq.com>
2020-11-29 10:35:56 +0800

    新增教师模块完善

--- src/main/java/com/ujiuye/mis/controller/TeacherController.java
+++ src/main/java/com/ujiuye/mis/controller/TeacherController.java
@@ -1,6 +1,8 @@
 package com.ujiuye.mis.controller;
 
 
+import com.ujiuye.mis.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeacherController {
 
 
+    @Autowired
+    private DeptService deptService;
+
+    @RequestMapping("/tea")
     public  String  teach(){
 
         System.out.println("哈哈哈");
