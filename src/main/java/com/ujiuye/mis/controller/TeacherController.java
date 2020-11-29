679476515ff72ef5fc7045cae3a6f9d582c8ec35
linxin-java <2451238975@qq.com>
2020-11-29 10:47:52 +0800

    新增了查找所有教师的方法

--- src/main/java/com/ujiuye/mis/controller/TeacherController.java
+++ src/main/java/com/ujiuye/mis/controller/TeacherController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/teach")
 public class TeacherController {
@@ -22,6 +24,15 @@ public class TeacherController {
          System.out.println("喜欢看你的笑容");
          System.out.println("喜欢洱海夕阳");
         return "林深时见鹿,海蓝时见鲸";
+    }
+
+
+
+    @RequestMapping("/find")
+    public String teachFind(){
+
+        System.out.println("查找所有教师");
+        return "找到了";
     }
 
 
